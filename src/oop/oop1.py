7f6a886de03a1366ee3a2887af2e44b7c7477ea2
Nathan <nateyl.b@gmail.com>
2020-06-05 11:59:21 -0700

    finished oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -11,9 +11,57 @@
 # Each class can simply "pass" for its body. The exercise is about setting up
 # the hierarchy.
 #
-# e.g.
+# e.g.s
 #
 # class Whatever:
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    # def __init__(self, name, description):
+    #     self.name = name
+    #     self.description = description
+    pass
+
+class GroundVehicle(Vehicle):
+    # def __init__(self, name, description):
+    #     super().__init__(name, description)
+    #     self.name = name
+    #     self.name = description
+    pass
+
+class Car(GroundVehicle):
+    # def __init__(self, name, description):
+    #     super().__init__(name, description)
+    #     self.name = name
+    #     self.name = description
+    pass
+
+class Motorcycle(GroundVehicle):
+    # def __init__(self, name, description):
+    #     super().__init__(name, description)
+    #     self.name = name
+    #     self.name = description
+    pass
+
+class FlightVehicle(Vehicle):
+    # def __init__(self, name, description):
+    #     super().__init__(name, description)
+    #     self.name = name
+    #     self.name = description
+    pass
+
+class Airplane(FlightVehicle):
+    # def __init__(self, name, description):
+    #     super().__init__(name, description)
+    #     self.name = name
+    #     self.name = description
+    pass
+
+class Starship(FlightVehicle):
+    # def __init__(self, name, description):
+    #     super().__init__(name, description)
+    #     self.name = name
+    #     self.name = description
+    pass
